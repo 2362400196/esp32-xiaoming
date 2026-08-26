9c34cdd2f0d8850b139cb60bfba90a84b3d5cfe3
zhuxiaohuanqn <2362400196@qq.com>
2026-08-26 20:04:29 +0800

    优化

--- vuepress-starter/docs/.vuepress/.temp/internal/home-hero-effects.js
+++
@@ -1,7 +0,0 @@
-import LiquidEther from '@theme/background/LiquidEther.vue'
-
-export const effectComponents = {
-  'liquid-ether': LiquidEther,
-}
-
-export const effects = Object.keys(effectComponents)
